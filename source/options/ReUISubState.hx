facdbb26f9761df47ed91b584f0ca3f530b94772
L <you@example.com>
2022-05-27 21:58:52 -0400

    first sussy commit

---
+++ source/options/ReUISubState.hx
@@ -0,0 +1,129 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class ReUISubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		var option:Option = new Option('Score Type:', "What should the score be like?", 'scoreType', 'string', 'Kade Engine',
+			['Kade Engine', 'Psych Engine', 'Disabled']);
+		addOption(option);
+
+		var option:Option = new Option('Score Style:', "What should the score look like?", 'scoreStyle', 'string', 'Random Engine',
+			['Random Engine', 'Psych Engine', 'Score Only']);
+		addOption(option);
+
+		var option:Option = new Option('Memory Counter', 'If unchecked, disables the memory counter.', 'memoryCounter', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Memory Counter Peak', 'If checked, adds the peak to the memory counter.', 'memoryCounterPeak', 'bool', false);
+                addOption(option);
+
+		var option:Option = new Option('Health Counter', 'If checked, enables the health counter.', 'healthCounter', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Judgements', 'If unchecked, hides judgements.', 'judgements', 'bool', true);
+		addOption(option);
+
+		var option:Option = new Option('Song Info', 'If unchecked, hides Song info.', 'songInfo', 'bool', true);
+		addOption(option);
+
+		var option:Option = new Option('Hitsound Volume', 'Funny note does \"Tick!\" when you hit it.', 'hitsoundVolume', 'percent', 0);
+		addOption(option);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+
+		var option:Option = new Option('MS Text', 'If unchecked, disables the ms text.', 'msTxt', 'bool', true);
+                addOption(option);
+
+		var option:Option = new Option('Ke Title Screen', 'If checked uses the ke title screen', 'keTitleScreen', 'bool', false);
+                addOption(option);
+
+		var option:Option = new Option('Normal Combo Count', 'If checked adds extra 0es to the combo counter.', 'normalCombo', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Detached Ratings', 'If checked, detaches the ratings from the camera.(independent from detached HUD)', 'detachedRatings',
+			'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Detached HUD', 'If checked, detaches the HUD (except the controls) from the camera.', 'detachedHUD', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Arrows Opacity', 'How Opaque should the arrows be.', 'arrowOpacity', 'percent', 1);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Lane Opacity', 'How Opaque should the lane underlay be.', 'laneOpacity', 'percent', 1);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Enemy Lane Opacity', 'How Opaque should the opponent lane be.', 'opponentLaneOpacity', 'percent', 0);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Enemy Arrows Opacity', 'How Opaque should the opponent arrows be.', 'opponentArrowOpacity', 'percent', 0);
+		option.scrollSpeed = 1.6;
+		option.minValue = 0.0;
+		option.maxValue = 1;
+		option.changeValue = 0.1;
+		option.decimals = 1;
+		addOption(option);
+
+		var option:Option = new Option('Pause Screen Song:', "What song do you prefer for the Pause Screen?", 'pauseMusic', 'string', 'Tea Time', ['None', 'Breakfast', 'Tea Time']);
+		addOption(option);
+		option.onChange = onChangePauseMusic;
+
+		super();
+	}
+
+	var changedMusic:Bool = false;
+	function onChangePauseMusic()
+	{
+		if(ClientPrefs.pauseMusic == 'None')
+			FlxG.sound.music.volume = 0;
+		else
+			FlxG.sound.playMusic(Paths.music(Paths.formatToSongPath(ClientPrefs.pauseMusic)));
+		changedMusic = true;
+	}
+}
---
+++ source/options/ReVisualsSubState.hx
@@ -0,0 +1,58 @@
+package options;
+
+#if desktop
+import Discord.DiscordClient;
+#end
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.addons.display.FlxGridOverlay;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.math.FlxMath;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import lime.utils.Assets;
+import flixel.FlxSubState;
+import flash.text.TextField;
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.util.FlxSave;
+import haxe.Json;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+import flixel.input.keyboard.FlxKey;
+import flixel.graphics.FlxGraphic;
+import Controls;
+
+using StringTools;
+
+class ReVisualsSubState extends BaseOptionsMenu
+{
+	public function new()
+	{
+		title = 'Visuals';
+		rpcTitle = 'Visuals Settings Menu'; // for Discord Rich Presence
+
+		var option:Option = new Option('Icon Boping', 'If checked enables icon boping.', 'iconBoping', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Tiny Fps Text', 'If checked, makes the fps text tiny, needs a restart.', 'tinyFpsTxt', 'bool', false);
+                addOption(option);
+
+		var option:Option = new Option('Rainbow FPS', 'If checked, makes the FPS have a chroma effect.', 'fpsRainbow', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Ms Text Colors', 'If unchecked, disables coloring in the Ms text.', 'coloredMs', 'bool', true);
+                addOption(option);
+
+		var option:Option = new Option('KE Timebar', 'If checked, uses the KE timebar.', 'keTimeBar', 'bool', false);
+		addOption(option);
+
+		var option:Option = new Option('Timebar Color', 'The color of your Timebar.', 'timeBarColor', 'string', 'White', 
+		['White', 'Blue', 'Cyan', 'Green', 'Red']);
+                addOption(option);
+
+		super();
+	}
+}
